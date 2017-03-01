dc8eba423b0ab43fa670c27ee11835b978ddb6f1
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2017-03-01 19:01:17 +0200

    finish

--- ColorCipher/src/ua/kas/main/Controller.java
+++ ColorCipher/src/ua/kas/main/Controller.java
@@ -2,16 +2,20 @@ package ua.kas.main;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 
 public class Controller {
 
@@ -19,9 +23,16 @@ public class Controller {
 	ColorPicker colorPickerFirst;
 	@FXML
 	ColorPicker colorPickerSecond;
+	@FXML
+	ColorPicker colorPickerDecFirst;
+	@FXML
+	ColorPicker colorPickerDecSecond;
 
 	@FXML
 	TextField tf_enterText;
+
+	private String path_txt;
+	private String path_image;
 
 	private ArrayList<String> alphabet = new ArrayList<>(Arrays.asList("а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и",
 			"й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э",
@@ -32,26 +43,26 @@ public class Controller {
 		Color colorFirst = colorPickerFirst.getValue();
 		Color colorSecond = colorPickerSecond.getValue();
 
-		int redFirst = (int) Math.round(colorFirst.getRed() * 255);
-		int greenFirst = (int) Math.round(colorFirst.getGreen() * 255);
-		int blueFirst = (int) Math.round(colorFirst.getBlue() * 255);
+		double redFirst = colorFirst.getRed() * 255;
+		double greenFirst = colorFirst.getGreen() * 255;
+		double blueFirst = colorFirst.getBlue() * 255;
 
-		int redSecond = (int) Math.round(colorSecond.getRed() * 255);
-		int greenSecond = (int) Math.round(colorSecond.getGreen() * 255);
-		int blueSecond = (int) Math.round(colorSecond.getBlue() * 255);
+		double redSecond = colorSecond.getRed() * 255;
+		double greenSecond = colorSecond.getGreen() * 255;
+		double blueSecond = colorSecond.getBlue() * 255;
 
 		double redCounts = Math.abs(redFirst - redSecond);
 		double greenCounts = Math.abs(greenFirst - greenSecond);
 		double blueCounts = Math.abs(blueFirst - blueSecond);
 
 		if (redCounts >= alphabet.size() || greenCounts >= alphabet.size() || blueCounts >= alphabet.size()) {
-			double redCoeff = redCounts / alphabet.size();
-			double greenCoeff = greenCounts / alphabet.size();
-			double blueCoeff = blueCounts / alphabet.size();
+			double redCoeff = redCounts / (alphabet.size() - 1);
+			double greenCoeff = greenCounts / (alphabet.size() - 1);
+			double blueCoeff = blueCounts / (alphabet.size() - 1);
 
-			int redLess = (redFirst > redSecond) ? redSecond : redFirst;
-			int greenLess = (greenFirst > greenSecond) ? greenSecond : greenFirst;
-			int blueLess = (blueFirst > blueSecond) ? blueSecond : blueFirst;
+			double redLess = (redFirst > redSecond) ? redSecond : redFirst;
+			double greenLess = (greenFirst > greenSecond) ? greenSecond : greenFirst;
+			double blueLess = (blueFirst > blueSecond) ? blueSecond : blueFirst;
 
 			try {
 				String text = tf_enterText.getText();
@@ -70,22 +81,122 @@ public class Controller {
 					index = alphabet.indexOf(letter);
 
 					image.setRGB(i, 0,
-							new java.awt.Color((int) (redLess + (index * redCoeff)),
-									(int) (greenLess + (index * greenCoeff)), (int) (blueLess + (index * blueCoeff)))
-											.getRGB());
-
-					System.out.println((int) (redLess + (index * redCoeff)) + " "
-							+ (int) (greenLess + (index * greenCoeff)) + " " + (int) (blueLess + (index * blueCoeff)));
-
+							new java.awt.Color((int) Math.round(redLess + (index * redCoeff)),
+									(int) Math.round(greenLess + (index * greenCoeff)),
+									(int) Math.round(blueLess + (index * blueCoeff))).getRGB());
 				}
 
-				ImageIO.write(image, "png", new File("res/image.png"));
+				ImageIO.write(image, "png", new File("image.png"));
 
 			} catch (Exception e) {
-				JOptionPane.showMessageDialog(null, "Enter text!");
+				JOptionPane.showMessageDialog(null, "Enter text! \n (а-я, \" \", 0-9) ");
 			}
 		} else {
 			JOptionPane.showMessageDialog(null, "Select other color!");
 		}
+	}
+
+	public void decod() throws IOException {
+		if (!(path_image.length() < 5) || !(path_txt.length() < 5)) {
+			Color colorFirst = colorPickerDecFirst.getValue();
+			Color colorSecond = colorPickerDecSecond.getValue();
+
+			double redFirst = colorFirst.getRed() * 255;
+			double greenFirst = colorFirst.getGreen() * 255;
+			double blueFirst = colorFirst.getBlue() * 255;
+
+			double redSecond = colorSecond.getRed() * 255;
+			double greenSecond = colorSecond.getGreen() * 255;
+			double blueSecond = colorSecond.getBlue() * 255;
+
+			double redCounts = Math.abs(redFirst - redSecond);
+			double greenCounts = Math.abs(greenFirst - greenSecond);
+			double blueCounts = Math.abs(blueFirst - blueSecond);
+
+			if (redCounts >= alphabet.size() || greenCounts >= alphabet.size() || blueCounts >= alphabet.size()) {
+				double redCoeff = redCounts / (alphabet.size() - 1);
+				double greenCoeff = greenCounts / (alphabet.size() - 1);
+				double blueCoeff = blueCounts / (alphabet.size() - 1);
+
+				double redLess = (redFirst > redSecond) ? redSecond : redFirst;
+				double greenLess = (greenFirst > greenSecond) ? greenSecond : greenFirst;
+				double blueLess = (blueFirst > blueSecond) ? blueSecond : blueFirst;
+
+				BufferedImage image = ImageIO.read(new File(path_image));
+				int pixel, red, green, blue;
+				int indexRed, indexGreen, indexBlue, biggest;
+				String text = "";
+
+				try {
+					for (int width = 0; width < image.getWidth(); width++) {
+						pixel = image.getRGB(width, 0);
+
+						// alpha = (pixel >> 24) & 0xff;
+						red = (pixel >> 16) & 0xff;
+						green = (pixel >> 8) & 0xff;
+						blue = (pixel >> 0) & 0xff;
+
+						indexRed = (int) Math.round((red - redLess) / redCoeff);
+						indexGreen = (int) Math.round((green - greenLess) / greenCoeff);
+						indexBlue = (int) Math.round((blue - blueLess) / blueCoeff);
+
+						biggest = (indexRed > indexGreen) ? indexRed : indexGreen;
+						biggest = (biggest > indexBlue) ? biggest : indexBlue;
+
+						text += alphabet.get(biggest);
+					}
+
+					writeFile(text);
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(null, "Enter text!");
+				}
+			} else {
+				JOptionPane.showMessageDialog(null, "Select other color!");
+			}
+		} else {
+			JOptionPane.showMessageDialog(null, "Select image and txt file!");
+		}
+	}
+
+	public void openImage(ActionEvent e) throws IOException {
+		FileChooser fileChooser = new FileChooser();
+
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Image (*.png)", "*.png");
+		fileChooser.getExtensionFilters().add(extFilter);
+
+		// Show save file dialog
+		File file = fileChooser.showOpenDialog(null);
+
+		path_image = file.getAbsolutePath();
+	}
+
+	public void selectTxt(ActionEvent e) throws IOException {
+		FileChooser fileChooser = new FileChooser();
+
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+		fileChooser.getExtensionFilters().add(extFilter);
+
+		// Show save file dialog
+		File file = fileChooser.showSaveDialog(null);
+		FileWriter fileWriter;
+
+		try {
+			fileWriter = new FileWriter(file);
+			fileWriter.close();
+
+			path_txt = file.getAbsolutePath();
+
+		} catch (Exception ex) {
+		}
+	}
+
+	private void writeFile(String text) throws IOException {
+		FileWriter fileWriter;
+		fileWriter = new FileWriter(new File(path_txt));
+
+		fileWriter.write(text);
+
+		fileWriter.flush();
+		fileWriter.close();
 	}
 }
