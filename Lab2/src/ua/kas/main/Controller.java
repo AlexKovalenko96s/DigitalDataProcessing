64d431a2ddae50933ca20fc0f7e33442c82cf4cd
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2017-03-13 17:35:35 +0200

    //

--- Lab2/src/ua/kas/main/Controller.java
+++ Lab2/src/ua/kas/main/Controller.java
@@ -4,12 +4,14 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
@@ -20,6 +22,9 @@ public class Controller {
 	ImageView imageV_1;
 	@FXML
 	ImageView imageV_2;
+
+	@FXML
+	TextField tf_v;
 
 	private String path_image;
 
@@ -62,6 +67,46 @@ public class Controller {
 		}
 
 		ImageIO.write(newImage, "png", new File("image.png"));
+
+		Image card = SwingFXUtils.toFXImage(newImage, null);
+
+		imageV_2.setImage(card);
+
+	}
+
+	public void window() throws IOException {
+		BufferedImage image = ImageIO.read(new File(path_image));
+		BufferedImage newImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+		Random random = new Random();
+
+		// Color color = null;
+		int pixel = 0, alpha, red, green, blue;
+		int x, y;
+
+		int v = Integer.parseInt(tf_v.getText());
+
+		for (int width = 0; width < image.getWidth(); width++) {
+			for (int height = 0; height < image.getHeight(); height++) {
+				// color = new Color(image.getRGB(width, height));
+
+				x = width + (int) ((random.nextDouble() - 0.5) * v);
+				y = height + (int) ((random.nextDouble() - 0.5) * v);
+
+				if (x >= 0 && x < image.getWidth() && y >= 0 && y < image.getHeight()) {
+					pixel = image.getRGB(x, y);
+
+					alpha = (pixel >> 24) & 0xff;
+					red = (pixel >> 16) & 0xff;
+					green = (pixel >> 8) & 0xff;
+					blue = (pixel >> 0) & 0xff;
+
+					newImage.setRGB(image.getWidth() - width - 1, height, new Color(red, green, blue, alpha).getRGB());
+				}
+			}
+		}
+
+		ImageIO.write(newImage, "png", new File("windiw.png"));
 
 		Image card = SwingFXUtils.toFXImage(newImage, null);
 
